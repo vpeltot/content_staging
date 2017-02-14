cd5984d4ea61c5ed69a57ec13bf18d4887ed5fe2
Vincent Peltot <vincent.peltot@niji.fr>
2017-02-14 07:49:48 +0100

    Add compatibility with paragraphs entities

---
+++ src/Plugin/migrate/process/ContentStagingIterator.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Drupal\content_staging\Plugin\migrate\process;
+
+use Drupal\migrate\Plugin\migrate\process\Iterator;
+use Drupal\migrate\MigrateExecutableInterface;
+use Drupal\migrate\Row;
+
+/**
+ * This plugin iterates and processes an array.
+ *
+ * @link https://www.drupal.org/node/2135345 Online handbook documentation for iterator process plugin @endlink
+ *
+ * @MigrateProcessPlugin(
+ *   id = "content_staging_iterator",
+ *   handle_multiples = TRUE
+ * )
+ */
+class ContentStagingIterator extends Iterator {
+
+  /**
+   * Runs a process pipeline on each destination property per list item.
+   */
+  public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
+    if (!is_array($value[0])) {
+      $new_value = [$value];
+    }
+    else {
+      $new_value = $value;
+    }
+    return parent::transform($new_value, $migrate_executable, $row, $destination_property);
+  }
+
+}
